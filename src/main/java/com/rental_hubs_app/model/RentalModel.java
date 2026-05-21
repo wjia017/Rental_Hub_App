fa66a44012e30d7fb2ce8c91f0559dd13e71e539
Lahana <lahana@Lahanas-MacBook-Pro.local>
2026-05-21 09:08:29 +0545

    Added the rental model for admin rentals

---
+++ src/main/java/com/rental_hubs_app/model/RentalModel.java
@@ -0,0 +1,91 @@
+package com.rental_hubs_app.model;
+
+import java.sql.Date;
+
+public class RentalModel {
+
+    private int rentalId;
+    private int userId;
+    private int clothId;
+    private String clothName;
+    private String username;
+    private Date rentalDate;
+    private Date returnDate;
+    private double totalPrice;
+    private String status;
+
+    public RentalModel() {
+    }
+
+    public int getRentalId() {
+        return rentalId;
+    }
+
+    public void setRentalId(int rentalId) {
+        this.rentalId = rentalId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getClothId() {
+        return clothId;
+    }
+
+    public void setClothId(int clothId) {
+        this.clothId = clothId;
+    }
+
+    public String getClothName() {
+        return clothName;
+    }
+
+    public void setClothName(String clothName) {
+        this.clothName = clothName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
