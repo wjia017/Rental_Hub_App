c4e669ca3c0bceb8ed028fc63b5eb8a94ac9f634
Lahana <lahana@Lahanas-MacBook-Pro.local>
2026-05-21 09:04:55 +0545

    Added the user model

---
+++ src/main/java/com/rental_hubs_app/model/UserModel.java
@@ -0,0 +1,118 @@
+package com.rental_hubs_app.model;
+
+public class UserModel {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String phone;
+    private String password;
+    private String role;
+    private String status;
+    private String address;
+    private String profileImage;
+
+    public UserModel() {
+    }
+
+    public UserModel(String firstName, String lastName, String username,
+                     String email, String phone, String password) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+}
